e566868deb88529ebac27dbdb69ae575a8aa43ec
Michelle <Michelle@DESKTOP-MO6GTI9.eduroam.hs-furtwangen.de>
2018-03-22 11:50:05 +0100

    Aufgabe

---
+++ Aufgabe-0/js/aufgabe0.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Bitte geben Sie hier Ihren Namen ein");
+    document.getElementById("greeting").innerHTML += name;
+});
+//# sourceMappingURL=aufgabe0.js.map
