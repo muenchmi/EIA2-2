9a1b63a13ff5250ae303fe7001e7695809892583
Michelle <Michelle@141.28.226.79>
2018-03-22 10:01:59 +0100

    Code geschrieben

--- Aufgabe-0/js/aufgabe0.ts
+++ Aufgabe-0/js/aufgabe0.ts
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     var name : string;
-    name = prompt("Bitte geben Sie Ihren Namen ein"); 
+    name = prompt("Bitte geben Sie hier Ihren Namen ein"); 
     document.getElementById("greeting").innerHTML += name; 
 });
