ab353c5254682f688e7203ead9ccb01fcccbdbc6
Michelle <Michelle@141.28.226.79>
2018-03-28 22:30:28 +0200

    boxes

--- Aufgabe-1/js/script.ts
+++ Aufgabe-1/js/script.ts
@@ -29,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
+
     function placeDiv(_color: string, _x: number, _y: number, _width: number, _height: number): void {
         let div: HTMLDivElement = document.createElement("div");
         document.body.appendChild(div);
@@ -43,3 +44,6 @@ document.addEventListener('DOMContentLoaded', function() {
         s.top = _y + "px";
     }
 });
+
+
+
