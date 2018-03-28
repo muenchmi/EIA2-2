b99e3d3c06b5528bc27eaf89e4af86eeeb999773
Michelle <Michelle@141.28.226.79>
2018-03-28 22:19:07 +0200

    boxes

--- Aufgabe-1/js/script.ts
+++ Aufgabe-1/js/script.ts
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function placeDiv(_color: string, _x: number, _y: number, _width: number, _height: number): void {
         let div: HTMLDivElement = document.createElement("div");
         document.body.appendChild(div);
-        
+
         let s: CSSStyleDeclaration = div.style;
         s.border = "thin solid black";
         s.position = "absolute";
