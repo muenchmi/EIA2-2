8b537bd496e77d261cbde2854b390e583c16b978
Michelle <Michelle@DESKTOP-MO6GTI9.hfu-open.fu.hs-furtwangen.de>
2018-04-11 19:43:43 +0200

    Test

--- Aufgabe-2/js/aufgabe2.js
+++ Aufgabe-2/js/aufgabe2.js
@@ -1,31 +1,11 @@
-var Aufgabe2;
-(function (Aufgabe2) {
-    window.addEventListener("load", init);
-    let playernumber = ["1", "2", "3", "4"];
-    let cardnumber = [5, 6, 7, 8];
-    let playernumberinputs = [];
-    //    let cardnumberinputs: HTMLInputElement[] = [];
-    //    
-    function init(_event) {
-        createplayernumber();
-        //        createcardnumber();
-    }
-    function createplayernumber() {
-        for (let i = 0; i < playernumber.length; i++) {
-            let label = document.createElement("label");
-            label.appendChild(document.createTextNode(playernumber[i]));
-            let input = document.createElement("input");
-            input.type = "number";
-            input.name = playernumber[i];
-            input.id = playernumber[i];
-            input.defaultValue = "1";
-            input.step = "1";
-            input.min = "1";
-            input.max = "4";
-            label.appendChild(input);
-            document.getElementById("playernumber").appendChild(label);
-            playernumberinputs.push(input);
-        }
-    }
-})(Aufgabe2 || (Aufgabe2 = {}));
+//namespace Aufgabe2 { }
+//
+//window.addEventListener("load", init);
+//
+//function init(_event: Event): void { 
+//
+//
+//}
+//
+//
 //# sourceMappingURL=aufgabe2.js.map
