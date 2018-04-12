1cd55b293ff1db62384a3afc11affa48fce89315
Michelle <Michelle@DESKTOP-MO6GTI9.hfu-open.fu.hs-furtwangen.de>
2018-04-12 13:00:23 +0200

    Aufgabe 2

--- Aufgabe-2/js/aufgabe2.js
+++ Aufgabe-2/js/aufgabe2.js
@@ -2,6 +2,18 @@ window.addEventListener("load", init);
 function init(_event) {
     let playerNumber = 2;
     let cardPair = 4;
+    let numberCards = cardPair * 2; //erstelle die doppelte anzahl der Kartenpaare 
     let creatCards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
+    let removeCard = creatCards.length - cardPair; // bestimme die Zahl die beim splice entfernt werden soll
+    creatCards.splice(numberCards, removeCard);
+    //shuffel
+    for (let i = 0; i < numberCards; i++) {
+        placeDiv();
+    }
+    function placeDiv() {
+        let div = document.createElement("div");
+        div.setAttribute("class", "cards");
+        document.body.appendChild(div);
+    }
 }
 //# sourceMappingURL=aufgabe2.js.map
--- Aufgabe-2/js/aufgabe2.ts
+++ Aufgabe-2/js/aufgabe2.ts
@@ -6,34 +6,29 @@ function init(_event: Event): void {
 
     let playerNumber: number = 2;
     let cardPair: number = 4;
-    let creatCards: string[] = ["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H","I","I","J","J","K","K","L","L","M","M","N","N","O","O","P","P","Q","Q","R","R","S","S","T","T","U","U","V","V","W","W","X","X","Y","Y","Z","Z"];
-    let removeCard: number = creatCards.length - cardPair
-    
-    creatCards.splice(cardPair, removeCard)
-    
+    let numberCards: number =  cardPair * 2 //erstelle die doppelte anzahl der Kartenpaare 
+    let creatCards: string[] = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
+    let removeCard: number = creatCards.length - cardPair // bestimme die Zahl die beim splice entfernt werden soll
+
+    creatCards.splice(numberCards, removeCard) 
+
     //shuffel
-    
-    for  (let i: number = 0; i < cardPair; i++){
-        placeDiv ()
-    
-    
-    
-    
-     function placeDiv( _x: number, _y: number, _width: number, _height: number): void {
+
+    for (let i: number = 0; i < numberCards; i++) {
+       placeDiv()
+    }
+
+
+    function placeDiv(): void {
         let div: HTMLDivElement = document.createElement("div");
+        div.setAttribute("class", "cards");
         document.body.appendChild(div);
 
-        let s: CSSStyleDeclaration = div.style;
-        s.border = "thin solid black";
-        s.position = "absolute";
-        s.backgroundColor = _color;
-        s.width = _width + "px";
-        s.height = _height + "px";
-        s.left = _x + "px";
-        s.top = _y + "px";
     }
     
-   
+    
+
+
 
 }
 
