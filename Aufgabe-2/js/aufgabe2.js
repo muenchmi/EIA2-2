d48e94ed69a041bdd963120a0db09a9e6e43fa7b
Michelle <Michelle@DESKTOP-MO6GTI9.eduroam.hs-furtwangen.de>
2018-04-15 16:00:34 +0200

    Aufgabe2

--- Aufgabe-2/js/aufgabe2.js
+++ Aufgabe-2/js/aufgabe2.js
@@ -1,19 +1,62 @@
-window.addEventListener("load", init);
-function init(_event) {
-    let playerNumber = 2;
-    let cardPair = 4;
-    let numberCards = cardPair * 2; //erstelle die doppelte anzahl der Kartenpaare 
-    let creatCards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
-    let removeCard = creatCards.length - cardPair; // bestimme die Zahl die beim splice entfernt werden soll
-    creatCards.splice(numberCards, removeCard);
-    //shuffel
-    for (let i = 0; i < numberCards; i++) {
-        placeDiv();
+var Aufgabe2;
+(function (Aufgabe2) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        let getPlayerNumber;
+        let getCardPair;
+        let playerNumber;
+        let cardPair;
+        let numberCards;
+        let creatCards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
+        let removeCard; // bestimme die Zahl die beim splice entfernt werden soll
+        getPlayerNumber = prompt("Bitte geben Sie hier die Spieleranzahl an (1-4 Spieler)", "");
+        getCardPair = prompt("Bitte geben Sie hier die Kartenpaare an (5-10 Paare)", "");
+        playerNumber = Number.parseInt(getPlayerNumber);
+        cardPair = Number.parseInt(getCardPair);
+        numberCards = cardPair * 2;
+        removeCard = creatCards.length - cardPair;
+        console.log(numberCards);
+        creatCards.splice(numberCards, removeCard); //nimm die unn�tigen Buchstaben aus dem array
+        //Anzahl der Karten im Game Div
+        for (let i = 0; i < numberCards; i++) {
+            placeDiv();
+        }
+        //Anzahl der Spieler im Players  
+        for (let i = 0; i < playerNumber; i++) {
+            let p = document.createElement("p");
+            let n = i + 1;
+            let text = "Spieler" + " " + n + ":";
+            p.innerText = text;
+            document.getElementById("players").appendChild(p);
+        }
+        function placeDiv() {
+            let random = creatCards[Math.floor(Math.random() * creatCards.length)]; //greife eine random Buchstabe aus dem array
+            let position = creatCards.indexOf(random); //finde die Position des Buchstabens herraus
+            let randomNumber = Math.random();
+            if (randomNumber < 0.6) {
+                let div = document.createElement("div");
+                div.setAttribute("class", random);
+                let s = div.style;
+                s.border = "thin solid black";
+                s.backgroundColor = "black";
+                document.getElementById("game").appendChild(div);
+            }
+            if (randomNumber > 0.6 && randomNumber < 0.8) {
+                let div = document.createElement("div");
+                div.setAttribute("class", random);
+                div.innerText = random;
+                let s = div.style;
+                s.border = "thin solid black";
+                document.getElementById("game").appendChild(div);
+            }
+            else if (randomNumber > 0.8) {
+                let div = document.createElement("div");
+                div.setAttribute("class", random);
+                let s = div.style;
+                document.getElementById("game").appendChild(div);
+            }
+            creatCards.splice(position, 1);
+        }
     }
-    function placeDiv() {
-        let div = document.createElement("div");
-        div.setAttribute("class", "cards");
-        document.body.appendChild(div);
-    }
-}
+})(Aufgabe2 || (Aufgabe2 = {}));
 //# sourceMappingURL=aufgabe2.js.map
