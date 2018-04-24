c2c63b3d02b1fc32aad124876247f316601acd21
Michelle <Michelle@DESKTOP-MO6GTI9.eduroam.hs-furtwangen.de>
2018-04-24 16:07:40 +0200

    Aufgabe 3

--- Aufgabe-3/js/aufgabe3.js
+++ Aufgabe-3/js/aufgabe3.js
@@ -1,15 +1,15 @@
 var Aufgabe3;
 (function (Aufgabe3) {
     window.addEventListener("load", init);
+    let getPlayerNumber;
+    let getCardPair;
+    let playerNumber;
+    let cardPair;
+    let numberCards;
+    let createCards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
+    let removeCard;
+    let counter = 0;
     function init(_event) {
-        let getPlayerNumber;
-        let getCardPair;
-        let playerNumber;
-        let cardPair;
-        let numberCards;
-        let createCards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
-        let removeCard;
-        let counter = 0;
         do {
             getPlayerNumber = prompt("Bitte geben Sie hier die Spieleranzahl an (1-4 Spieler)", "");
             playerNumber = Number.parseInt(getPlayerNumber);
@@ -32,51 +32,51 @@ var Aufgabe3;
             p.innerText = text;
             document.getElementById("players").appendChild(p);
         }
-        function createCard() {
-            let random = createCards[Math.floor(Math.random() * createCards.length)];
-            let position = createCards.indexOf(random);
-            let div = document.createElement("div");
-            div.setAttribute("class", "hidden");
-            div.innerText = random;
-            div.addEventListener("click", cardClicked);
-            document.getElementById("game").appendChild(div);
-            createCards.splice(position, 1);
-        }
-        function cardClicked(event) {
-            let clicked = event.target;
-            if (counter < 2) {
-                counter++;
-                if (counter == 1) {
-                    clicked.setAttribute("class", "open");
-                }
-                else {
-                    clicked.setAttribute("class", "open");
-                    setTimeout(compareCards, 2000);
-                }
-            }
-        }
-        function compareCards() {
-            let firstGuess = document.getElementsByClassName("open")[0];
-            let secondGuess = document.getElementsByClassName("open")[1];
-            console.log(firstGuess);
-            console.log(secondGuess);
-            if (firstGuess.innerHTML == secondGuess.innerHTML) {
-                firstGuess.setAttribute("class", "taken");
-                secondGuess.setAttribute("class", "taken");
-                counter = 0;
+    }
+    function createCard() {
+        let random = createCards[Math.floor(Math.random() * createCards.length)];
+        let position = createCards.indexOf(random);
+        let div = document.createElement("div");
+        div.setAttribute("class", "hidden");
+        div.innerText = random;
+        div.addEventListener("click", cardClicked);
+        document.getElementById("game").appendChild(div);
+        createCards.splice(position, 1);
+    }
+    function cardClicked(event) {
+        let clicked = event.target;
+        if (counter < 2) {
+            counter++;
+            if (counter == 1) {
+                clicked.setAttribute("class", "open");
             }
             else {
-                firstGuess.setAttribute("class", "hidden");
-                secondGuess.setAttribute("class", "hidden");
-                counter = 0;
+                clicked.setAttribute("class", "open");
+                setTimeout(compareCards, 2000);
             }
-            fin();
         }
-        function fin() {
-            let hiddencards = document.getElementsByClassName("hidden");
-            if (hiddencards.length == 0) {
-                alert("Gut gemacht! Du hast das Spiel beendet.");
-            }
+    }
+    function compareCards() {
+        let firstGuess = document.getElementsByClassName("open")[0];
+        let secondGuess = document.getElementsByClassName("open")[1];
+        console.log(firstGuess);
+        console.log(secondGuess);
+        if (firstGuess.innerHTML == secondGuess.innerHTML) {
+            firstGuess.setAttribute("class", "taken");
+            secondGuess.setAttribute("class", "taken");
+            counter = 0;
+        }
+        else {
+            firstGuess.setAttribute("class", "hidden");
+            secondGuess.setAttribute("class", "hidden");
+            counter = 0;
+        }
+        fin();
+    }
+    function fin() {
+        let hiddencards = document.getElementsByClassName("hidden");
+        if (hiddencards.length == 0) {
+            alert("Gut gemacht! Du hast das Spiel beendet.");
         }
     }
 })(Aufgabe3 || (Aufgabe3 = {}));
--- Aufgabe-3/js/aufgabe3.ts
+++ Aufgabe-3/js/aufgabe3.ts
@@ -2,17 +2,17 @@ namespace Aufgabe3 {
 
     window.addEventListener("load", init);
 
+    let getPlayerNumber: string;
+    let getCardPair: string;
+    let playerNumber: number;
+    let cardPair: number;
+    let numberCards: number;
+    let createCards: string[] = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
+    let removeCard: number;
+
+    let counter: number = 0;
+    
     function init(_event: Event): void {
-
-        let getPlayerNumber: string;
-        let getCardPair: string;
-        let playerNumber: number;
-        let cardPair: number;
-        let numberCards: number;
-        let createCards: string[] = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L", "M", "M", "N", "N", "O", "O", "P", "P", "Q", "Q", "R", "R", "S", "S", "T", "T", "U", "U", "V", "V", "W", "W", "X", "X", "Y", "Y", "Z", "Z"];
-        let removeCard: number; 
-
-        let counter: number = 0;
 
         do {
             getPlayerNumber = prompt("Bitte geben Sie hier die Spieleranzahl an (1-4 Spieler)", "");
@@ -32,8 +32,6 @@ namespace Aufgabe3 {
         console.log(numberCards);
         createCards.splice(numberCards, removeCard);
 
-       
-
         for (let i: number = 0; i < numberCards; i++) {
             createCard();
         }
@@ -46,67 +44,68 @@ namespace Aufgabe3 {
             document.getElementById("players").appendChild(p);
 
         }
+    }
+    
+    function createCard(): void {
+        let random: string = createCards[Math.floor(Math.random() * createCards.length)];
+        let position: number = createCards.indexOf(random);
 
-        function createCard(): void {
-            let random: string = createCards[Math.floor(Math.random() * createCards.length)]; 
-            let position: number = createCards.indexOf(random); 
+        let div: HTMLDivElement = document.createElement("div");
 
-            let div: HTMLDivElement = document.createElement("div");
+        div.setAttribute("class", "hidden");
+        div.innerText = random;
+        div.addEventListener("click", cardClicked);
+        document.getElementById("game").appendChild(div);
+        createCards.splice(position, 1);
+    }
 
-            div.setAttribute("class", "hidden");
-            div.innerText = random;
-            div.addEventListener("click", cardClicked);
-            document.getElementById("game").appendChild(div);
-            createCards.splice(position, 1);
-        }
+    function cardClicked(event: Event): void {
+        let clicked: HTMLElement = <HTMLElement>event.target;
 
-        function cardClicked(event: Event): void {
-            let clicked: HTMLElement = <HTMLElement>event.target;
+        if (counter < 2) {
+            counter++;
+            if (counter == 1) {
+                clicked.setAttribute("class", "open");
 
-            if (counter < 2) {
-                counter++;
-                if (counter == 1) {
-                    clicked.setAttribute("class", "open");
-
-                }
-                else {
-                    clicked.setAttribute("class", "open");
-                    setTimeout(compareCards, 2000);
-                }
             }
-        }
-        
-        function compareCards(): void {
-            let firstGuess: any = document.getElementsByClassName("open")[0];
-            let secondGuess: any = document.getElementsByClassName("open")[1];
-
-            console.log(firstGuess);
-            console.log(secondGuess);
-
-            if (firstGuess.innerHTML == secondGuess.innerHTML) {
-
-                firstGuess.setAttribute("class", "taken");
-                secondGuess.setAttribute("class", "taken");
-
-                counter = 0;
-            }
-
             else {
-                firstGuess.setAttribute("class", "hidden");
-                secondGuess.setAttribute("class", "hidden");
-                counter = 0;
-            }
-
-            fin();
-        }
-        
-        function fin(): void {
-            let hiddencards: any = document.getElementsByClassName("hidden");
-            if (hiddencards.length == 0) {
-                alert("Gut gemacht! Du hast das Spiel beendet.");
+                clicked.setAttribute("class", "open");
+                setTimeout(compareCards, 2000);
             }
         }
     }
+
+    function compareCards(): void {
+        let firstGuess: any = document.getElementsByClassName("open")[0];
+        let secondGuess: any = document.getElementsByClassName("open")[1];
+
+        console.log(firstGuess);
+        console.log(secondGuess);
+
+        if (firstGuess.innerHTML == secondGuess.innerHTML) {
+
+            firstGuess.setAttribute("class", "taken");
+            secondGuess.setAttribute("class", "taken");
+
+            counter = 0;
+        }
+
+        else {
+            firstGuess.setAttribute("class", "hidden");
+            secondGuess.setAttribute("class", "hidden");
+            counter = 0;
+        }
+
+        fin();
+    }
+
+    function fin(): void {
+        let hiddencards: any = document.getElementsByClassName("hidden");
+        if (hiddencards.length == 0) {
+            alert("Gut gemacht! Du hast das Spiel beendet.");
+        }
+    }
+
 }
 
 
