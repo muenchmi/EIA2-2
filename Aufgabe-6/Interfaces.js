e79e80a1e3cd9a45ce33e72e3a3815726619888b
Michelle <Michelle@DESKTOP-MO6GTI9.hfu-open.fu.hs-furtwangen.de>
2018-06-06 17:58:52 +0200

    Aufgabe7

---
+++ Aufgabe-6/Interfaces.js
@@ -0,0 +1,6 @@
+var Aufgabe06_Interfaces;
+(function (Aufgabe06_Interfaces) {
+    // Homogenes assoziatives Array zur Speicherung eines Studenten unter der Matrikelnummer
+    Aufgabe06_Interfaces.studiHomoAssoc = {};
+})(Aufgabe06_Interfaces || (Aufgabe06_Interfaces = {}));
+//# sourceMappingURL=Interfaces.js.map
