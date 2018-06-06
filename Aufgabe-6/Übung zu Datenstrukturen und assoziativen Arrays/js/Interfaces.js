a4d4c4dd0cc8ca9ba769cbb04458b0543ff55d26
Michelle <Michelle@DESKTOP-MO6GTI9.hfu-open.fu.hs-furtwangen.de>
2018-06-06 17:50:22 +0200

    Aufgabe6

--- Aufgabe-6/Übung zu Datenstrukturen und assoziativen Arrays/js/Interfaces.js
+++
@@ -1,6 +0,0 @@
-var L06_Interfaces;
-(function (L06_Interfaces) {
-    // Homogenes assoziatives Array zur Speicherung eines Studenten unter der Matrikelnummer
-    L06_Interfaces.studiHomoAssoc = {};
-})(L06_Interfaces || (L06_Interfaces = {}));
-//# sourceMappingURL=Interfaces.js.map
