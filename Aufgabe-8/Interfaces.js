71f6509ef0a02ef128528b671f3b81d8830222cd
Michelle <Michelle@DESKTOP-MO6GTI9.eduroam.hs-furtwangen.de>
2018-06-07 12:26:31 +0200

    Aufgabe8

---
+++ Aufgabe-8/Interfaces.js
@@ -0,0 +1,6 @@
+var Aufgabe07_Interfaces;
+(function (Aufgabe07_Interfaces) {
+    // Homogenes assoziatives Array zur Speicherung eines Studenten unter der Matrikelnummer
+    Aufgabe07_Interfaces.studiHomoAssoc = {};
+})(Aufgabe07_Interfaces || (Aufgabe07_Interfaces = {}));
+//# sourceMappingURL=Interfaces.js.map
---
+++ Aufgabe-8/Interfaces.ts
@@ -0,0 +1,18 @@
+namespace Aufgabe07_Interfaces {
+     // Struktur des heterogenen assoziativen Arrays als Datensatz für einen Studenten
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        studiengang: string;
+    }
+
+    // Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+    export interface Studis {         [matrikel: string]: Studi;
+    }
+ 
+    // Homogenes assoziatives Array zur Speicherung eines Studenten unter der Matrikelnummer
+    export let studiHomoAssoc: Studis = {};  
+}
