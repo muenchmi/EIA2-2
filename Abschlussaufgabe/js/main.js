4ca97f0f77154c1ade6ec9277268d0ea74f29190
Michelle <Michelle@DESKTOP-MO6GTI9.eduroam.hs-furtwangen.de>
2018-07-05 14:08:04 +0200

    Abschlussaufgabe

--- Abschlussaufgabe/js/main.js
+++ Abschlussaufgabe/js/main.js
@@ -3,17 +3,13 @@ var Abschlussaufgabe;
     window.addEventListener("load", init);
     let bubblenumber = 8;
     let fishnumber = 7;
+    var image = new Image();
+    image.src = 'images/yosemite.jpg';
     function init(_event) {
         Abschlussaufgabe.canvas = document.getElementsByTagName("canvas")[0];
-        Abschlussaufgabe.canvas.width = 1500;
-        Abschlussaufgabe.canvas.height = 1100;
+        Abschlussaufgabe.canvas.width = 1093;
+        Abschlussaufgabe.canvas.height = 756;
         Abschlussaufgabe.crc2 = Abschlussaufgabe.canvas.getContext("2d");
-        //        drawWater(canvas.width, canvas.height);
-        //        drawSeaweed(canvas.width, canvas.height);
-        //        drawSand(canvas.width, canvas.height);
-        //        drawStones(canvas.width, canvas.height);
-        //        drawSeashell(canvas.width, canvas.height);
-        //      RANDOM BUBBLE
         for (let i = 0; i < bubblenumber; i++) {
             let randomBubbles = Math.floor((Math.random() * 2) + 0);
             let x = Math.floor((Math.random() * (Abschlussaufgabe.canvas.width / 2 - 350)) + 100);
--- Abschlussaufgabe/js/main.ts
+++ Abschlussaufgabe/js/main.ts
@@ -4,31 +4,26 @@ namespace Abschlussaufgabe {
     export let canvas: HTMLCanvasElement;
     let bubblenumber: number = 8;
     let fishnumber: number = 7;
+    var image = new Image();
+    image.src = 'images/yosemite.jpg';
 
     function init(_event: Event): void {
         canvas = document.getElementsByTagName("canvas")[0];
-        canvas.width = 1500;
-        canvas.height = 1100;        
+        canvas.width = 1093;
+        canvas.height = 756;
         crc2 = canvas.getContext("2d");
 
-//        drawWater(canvas.width, canvas.height);
-//        drawSeaweed(canvas.width, canvas.height);
-//        drawSand(canvas.width, canvas.height);
-//        drawStones(canvas.width, canvas.height);
-//        drawSeashell(canvas.width, canvas.height);
 
-
-        //      RANDOM BUBBLE
         for (let i: number = 0; i < bubblenumber; i++) {
             let randomBubbles: number = Math.floor((Math.random() * 2) + 0);
             let x: number = Math.floor((Math.random() * (canvas.width / 2 - 350)) + 100);
             let y: number = Math.floor((Math.random() * (canvas.height - 500)) + 50);
             switch (randomBubbles) {
                 case 0:
-//                    drawsmallBubble(x, y);
+                    //                    drawsmallBubble(x, y);
                     break;
                 case 1:
-//                    drawBubble(x, y);
+                    //                    drawBubble(x, y);
                     break;
             }
         }
@@ -40,10 +35,10 @@ namespace Abschlussaufgabe {
             let y: number = Math.floor((Math.random() * (canvas.height - 250)) + 50);
             switch (randomFishs) {
                 case 0:
-//                    drawFishOne(x, y);
+                    //                    drawFishOne(x, y);
                     break;
                 case 1:
-//                    drawFishTwo(x, y);
+                    //                    drawFishTwo(x, y);
                     break;
             }
         }
